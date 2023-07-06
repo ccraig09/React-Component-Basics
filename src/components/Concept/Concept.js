4bc12b2ac9ba8045ba21be58dfb309a033f86b69
Carlos Craig <carlos.craig09@gmail.com>
2023-07-05 18:18:20 -0700

    Initial commit

---
+++ src/components/Concept/Concept.js
@@ -0,0 +1,9 @@
+export default function Concept(props) {
+  return (
+    <li className="concept">
+      <img src={props.image} alt={props.image} />
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </li>
+  );
+}
